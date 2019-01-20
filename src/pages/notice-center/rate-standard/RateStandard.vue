416b4cdb8111e3370e2c0ef7e1a44b99540fcf20
sundandan <1486620982@qq.com>
2019-01-20 17:30:09 +0800

    补充公告中心页面

--- src/pages/notice-center/rate-standard/RateStandard.vue
+++ src/pages/notice-center/rate-standard/RateStandard.vue
@@ -7,21 +7,20 @@
   <div class="recharge-rate-standard">
     <h3>费率标准</h3>
     <template>
-      <el-tabs v-model="activeName2" type="card" @tab-click="handleClick">
+      <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
         <el-tab-pane label="交易费率" name="first">
           <el-table
             :data="tableData"
             style="width: 100%">
-            <el-table-column
-              prop="date"
-              label="日期"
-              width="180">
-            </el-table-column>
-            <el-table-column
-              prop="name"
-              label="姓名"
-              width="180">
-            </el-table-column>
+          <el-table-column
+            prop="currency"
+            label="币种"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="procedures"
+            label="提币手续">
+          </el-table-column>
         </el-table>
         </el-tab-pane>
         <el-tab-pane label="提币费率" name="second">
@@ -29,18 +28,18 @@
           :data="tableData"
           style="width: 100%">
           <el-table-column
-            prop="date"
-            label="日期"
+            prop="currency"
+            label="币种"
             width="180">
           </el-table-column>
           <el-table-column
-            prop="name"
-            label="姓名"
+            prop="mininum"
+            label="提币最小量"
             width="180">
           </el-table-column>
           <el-table-column
-            prop="address"
-            label="地址">
+            prop="procedures"
+            label="提币手续">
           </el-table-column>
         </el-table>
         </el-tab-pane>
@@ -49,7 +48,25 @@
   </div>
 </div>
 </template>
-<script></script>
+<script>
+export default {
+  data () {
+    return {
+      activeName: 'first',
+      tableData: [{
+        currency: 'USDT',
+        procedures: '0.2%',
+        mininum: '100'
+      }]
+    }
+  },
+  methods: {
+    handleClick (tab, event) {
+      console.log(tab, event)
+    }
+  }
+}
+</script>
 <style lang="scss">
     @import '../../../assets/scss/global';
 </style>
